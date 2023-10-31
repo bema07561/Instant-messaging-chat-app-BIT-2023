50f3e78d0a484c8859255bbaf619cc952972d155
bema07561 <94055763+bema07561@users.noreply.github.com>
2023-10-31 14:19:50 +0800

    Delete website directory

--- website/main.js
+++
@@ -1,8 +0,0 @@
-const sendMessage = document.querySelector('.clickable');
-
-function send(){
-    let a = document.querySelector(".write-message").value;
-    console.log(a);
-}
-
-sendMessage.onclick = send;
